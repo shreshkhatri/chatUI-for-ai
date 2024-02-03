7ea6b1000821eb2d29af3dae91d30bcf8412d9aa
shreshkhatri <shresh.khatri@gmail.com>
2024-02-03 16:59:21 +0000

    unnecessary import removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-import { Box } from "@mui/joy";
-import MobileFriendlyDrawer from "./components/client-components/MobileFriendlyDrawerMobileFriendlyDrawer";
 import MainChatUI from "./components/client-components/MainChatUI";
 
 export default function Home() {
